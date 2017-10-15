e11003eeb06952c15bb6fc5b98a2ed590e631a26
unknown <ljlovemichael@hotmail.com>
2017-10-15 23:12:12 +0800

    search in rotated sorted array 2

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,11 +1,11 @@
-#include "searchinarray.h"
+#include "searchinarray2.h"
 #include <iostream>
 #include <vector>
 using namespace std;
 int main()
 {
     Solution r;
-    vector<int> v={1};
-    int n=r.search(v,0);
+    vector<int> v={1,3,1,1};
+    bool n=r.search(v,3);
     printf("%d\n",n);
 }
